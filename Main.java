51fa9d1be7df930fc7421b6ca79f039a892d8b77
nilsJavaNoob <nils1968@gmail.com>
2018-09-15 00:22:07 +0300

    0.22

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 public class Main {
 
 	 public static void main(String[] args){
-			System.out.println("Hi");
+			System.out.println("Hu");
 			Owner owner = new Owner("Vasya");
 			House house = new House(16,4);
+			Appartment a = new Appartment(2);
+			
 			house.settle(owner);
 			
 			System.out.println("=============");
 			System.out.println(house.toString());
+			System.out.println(a.toString());
 			//System.out.println(owner.toString());
 			System.out.println("=============");
 	}
