5c4ed72ba642b9bacba00b0615ddf6413c5de5bd
nilsJavaNoob <nils1968@gmail.com>
2018-09-28 22:01:05 +0300

    --

--- House.java
+++ House.java
@@ -1,35 +1,10 @@
 public class House {
-	Floor[] floors;
-	
-	public House(int floorCount,  int appartmentCount){
-		NumberGenerator numberGenerator = new NumberGenerator();
-		floors = new Floor[floorCount];
-		for(int i = 0;  i<floorCount; i++){
-			floors[i] = new Floor(i+1,appartmentCount,numberGenerator);
-		}
-	}
-	
-	// tmp_house_vizual
-	public String toString(){
-		String result = "House\n";
-		for(Floor floor : floors){
-			result+=floor.toString()+"\n";
-		}
-		return result;
-	}
-//===========================================
 
 
-	public void settle(Owner owner){
-		for(Floor floor:floors){
-			Appartment app = floor.getFreeAppartment();
-			//app exists
-			if(app !=null){
-				app.addOwner(owner);
-				break;
-			}
-			//throw RunTimeException
+		public String toString(){
+			String result = " ----- House -----\n";
+			result+= "=====================\n";
+			result+= "=====================\n";
+			return result;
 		}
-	}//settle
-	
 	}//class
